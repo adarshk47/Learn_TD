c98522d36c311dd331e3a51594f5e1bfd8dc53d9
adarshk47 <aadarshk627@gmail.com>
2026-05-28 07:41:44 +0530

    i

--- nse_dashboard/config.py
+++ nse_dashboard/config.py
@@ -1,8 +1,9 @@
-ANGEL_ONE = {
-    "api_key":     "LkKs5NJG",
-    "secret_key":  "600734be-7bf2-4bfe-a00c-a5972673d16d",
-    "client_id":   "A114064",
-    "password":    "Mahadev1@#",   # Angel One login password
-    "mpin":        "1008",          # 4-digit MPIN used by SmartAPI generateSession
-    "totp_secret": "6IK5P2KWF3YULRMR6VSUVZZVLI",
-}
+[angel_one]
+api_key     = "LkKs5NJG"
+secret_key  = "600734be-7bf2-4bfe-a00c-a5972673d16d"
+client_id   = "A114064"
+password    = "Mahadev1@#"
+mpin        = "1008"
+totp_secret = "6IK5P2KWF3YULRMR6VSUVZZVLI"
+
+anthropic_api_key = "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"
