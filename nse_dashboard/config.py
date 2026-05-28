6afddbd7cbb8d9f3b1322233b23028001b1e87ad
adarshk47 <aadarshk627@gmail.com>
2026-05-28 07:45:48 +0530

    Remove anthropic_api_key from config.py

--- nse_dashboard/config.py
+++ nse_dashboard/config.py
@@ -5,5 +5,3 @@ client_id   = "A114064"
 password    = "Mahadev1@#"
 mpin        = "1008"
 totp_secret = "6IK5P2KWF3YULRMR6VSUVZZVLI"
-
-anthropic_api_key = "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"
